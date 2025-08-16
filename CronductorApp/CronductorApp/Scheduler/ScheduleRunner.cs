ba96e01c45dcb8db84737a31aaaca72b76ee301f
James Johnson <jamesjohnson88@gmail.com>
2025-08-16 16:57:31 +0100

    Refactor out of prototype and towards a more realistic approach

---
+++ CronductorApp/CronductorApp/Scheduler/ScheduleRunner.cs
@@ -0,0 +1,36 @@
+﻿
+namespace CronductorApp.Scheduler;
+
+public class ScheduleRunner : BackgroundService
+{
+    private PriorityQueue<ScheduledRequest, DateTime> _scheduleQueue = new();
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        // todo - clear down PQ
+        
+        return Task.CompletedTask;
+    }
+
+    private static void ExecuteScheduledTasks(object? state)
+    {
+        if (state is not ScheduledRequest scheduledRequest)
+        {
+            Console.WriteLine("Invalid schedule state.");
+            return;
+        }
+        
+        Console.WriteLine($"Executed {scheduledRequest.Name} at {DateTime.Now}");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // dunno what to do with this?
+        return Task.CompletedTask;
+    }
+}
---
+++ CronductorApp/CronductorApp/Scheduler/ScheduledRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CronductorApp.Scheduler;
+
+public record ScheduledRequest(string Name, int FrequencySeconds)
+{
+    public string Name { get; set; } = Name;
+
+    public int FrequencySeconds { get; set; } = FrequencySeconds;
+}
