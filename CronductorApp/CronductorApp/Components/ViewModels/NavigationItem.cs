3dc5d1b2a7282fda4be873b64fc79517e1dda1cf
James Johnson <jamesjohnson88@gmail.com>
2025-09-02 22:37:09 +0100

    Nav done for now...

---
+++ CronductorApp/CronductorApp/Components/ViewModels/NavigationItem.cs
@@ -0,0 +1,8 @@
+namespace CronductorApp.Components.ViewModels;
+
+public class NavigationItem
+{
+    public required string Title { get; init; }
+    public required string Link { get; init; }
+    public string? Icon { get; init; }
+}
