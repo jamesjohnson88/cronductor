261c947c6b14f86b60b99247c33506a725604780
James Johnson <jamesjohnson88@gmail.com>
2025-09-03 12:00:55 +0100

    Enhance nav with active state management and styling improvements

--- CronductorApp/CronductorApp/Components/ViewModels/NavigationItem.cs
+++ CronductorApp/CronductorApp/Components/ViewModels/NavigationItem.cs
@@ -5,4 +5,5 @@ public class NavigationItem
     public required string Title { get; init; }
     public required string Link { get; init; }
     public string? Icon { get; init; }
+    public bool IsActive { get; set; } = false;
 }
